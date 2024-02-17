7b053ade68cab2f9f6f5031e544d9e57e0f27ea4
Daria Orlova <da.orlova365@gmail.com>
2024-02-17 05:44:31 +0300

    Initial Commit

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,23 @@
 public class Task2 {
+    public static void main(String[] args) {
+        int clientOS = 1;
+        int clientDeviceYear = 2000;
+        appVersion(clientOS, clientDeviceYear);
+    }
+
+    public static void appVersion(int clientOS, int clientDeviceYear) {
+        if (clientOS == 0) {
+            if (clientDeviceYear <= 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else {
+            if (clientDeviceYear <= 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+    }
 }
--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,20 @@
 public class Task3 {
+    public static void main(String[] args) {
+        int deliveryDistance = 21;
+        if (deliveryDistance <= 100) {
+            Task3.deliveryDays(67);
+        }
+    }
+
+    public static int deliveryDays(int deliveryDistance) {
+        int days = 1;
+
+        if (deliveryDistance > 20) {
+            days++;
+        }
+        if (deliveryDistance > 60) {
+            days++;
+        }
+        return days;
+    }
 }
